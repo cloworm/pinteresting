11707653f2d39dad4ca1216e1e8474ebc66da50a
cloworm <rubyquin@gmail.com>
2016-02-06 19:51:58 +0000

    Added styling to home

---
+++ app/assets/javascripts/materialize_initializers.js
@@ -0,0 +1,20 @@
+var ready = function() {
+  $(".button-collapse").sideNav();
+  $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+  });
+  $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+// Using document.ready with turbo-links http://stackoverflow.com/a/18770589
