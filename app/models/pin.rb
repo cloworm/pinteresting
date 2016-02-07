926c20eaece0fd7b16c98b44efec8f1f0e2e8430
cloworm <rubyquin@gmail.com>
2016-02-07 08:32:04 -0500

    Used https for images in production

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,9 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image,
+    styles: { medium: "300x300>", thumb: "100x100>" },
+    :s3_protocol => Rails.env.production? ? :https : :http
+
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
