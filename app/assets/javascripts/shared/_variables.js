0b838ae389b3926877b9fbea3408281539638d07
TSMMark <mark.allen@vydia.com>
2016-02-08 22:07:26 -0500

    introduce tiny screen size
    and shared/variables for js

---
+++ app/assets/javascripts/shared/_variables.js
@@ -0,0 +1,8 @@
+window.tinyScreen = 469;
+window.smallScreen = 600;
+window.mediumScreen = 992;
+window.largeScreen = 1200;
+window.tinyScreenUp = 470;
+window.smallScreenUp = 601;
+window.mediumScreenUp = 993;
+window.largeScreenUp = 1201;
