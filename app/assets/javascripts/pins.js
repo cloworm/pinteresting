b8ec66e785ee94f6de027bc95dd3efea9a112515
TSMMark <mark.allen@vydia.com>
2016-02-08 22:07:52 -0500

    no masonry on small screens

---
+++ app/assets/javascripts/pins.js
@@ -0,0 +1,34 @@
+var masonryIsEnabled = false;
+
+var shouldMasonry = function () {
+  return $(window).width() > tinyScreenUp;
+}
+
+var initMasonry = _.debounce(function () {
+  var $pins = $("#pins");
+
+  if (shouldMasonry()) {
+    if (!masonryIsEnabled) {
+      masonryIsEnabled = true;
+      $pins.masonry({
+        itemSelector: '.box',
+        isFitWidth: true
+      });
+    }
+  }
+  else if (masonryIsEnabled) {
+    masonryIsEnabled = false;
+    $pins.masonry('destroy');
+    $pins.css({ width: "auto", height: "auto" });
+  }
+}, 100);
+
+var onLoad = function () {
+  initMasonry();
+  $('#pins').imagesLoaded(initMasonry);
+}
+
+$(onLoad);
+// $(document).on('page:load', initMasonry) // jquery.turbolinks handles this for us.
+
+$(window).on("resize", initMasonry);
