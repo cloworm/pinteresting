33c731c1ad94c42b20afcf5466587d875f546211
TSMMark <mark.allen@vydia.com>
2016-02-09 22:28:11 -0500

    running migrate updated the schema
    consequence of different rails versions? across machines?

--- db/schema.rb
+++ db/schema.rb
@@ -14,24 +14,24 @@
 ActiveRecord::Schema.define(version: 20160210001414) do
 
   create_table "activities", force: :cascade do |t|
-    t.string   "body"
-    t.integer  "pin_id",     null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id",    null: false
-    t.string   "type",       null: false
+    t.string   "body",       limit: 255
+    t.integer  "pin_id",                 null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id",                null: false
+    t.string   "type",       limit: 255, null: false
   end
 
   add_index "activities", ["pin_id"], name: "index_activities_on_pin_id"
   add_index "activities", ["user_id"], name: "index_activities_on_user_id"
 
   create_table "pins", force: :cascade do |t|
-    t.string   "description"
+    t.string   "description",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
@@ -59,19 +59,19 @@ ActiveRecord::Schema.define(version: 20160210001414) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
