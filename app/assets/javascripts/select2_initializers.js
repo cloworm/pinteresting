9b6037216e3d66795301bbf8b815f082fea1e161
TSMMark <mark.allen@vydia.com>
2016-02-09 23:11:11 -0500

    tags show page
    and select2 skeleton

---
+++ app/assets/javascripts/select2_initializers.js
@@ -0,0 +1,6 @@
+$(function () {
+  // TODO: get select2 working properly.
+  // $("#pin_tag_list").select2({
+  //   "tags": true
+  // });
+});
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,8 @@
 class TagsController < ApplicationController
 
   def show
-    @tag =  ActsAsTaggableOn::Tag.find(params[:id])
-    @posts = Post.tagged_with(@tag.name)
+    @tag =  ActsAsTaggableOn::Tag.find_by_name(params[:id])
+    @pins = Pin.tagged_with(@tag.name).all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10).includes(:activities)
   end
 
 end
