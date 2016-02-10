d4f9e747704d0e1af3884af307f5c3978d0e4788
TSMMark <mark.allen@vydia.com>
2016-02-09 22:28:27 -0500

    remove unused tags index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,8 @@
 class TagsController < ApplicationController
-  def index
-    @tags = ActsAsTaggableOn::Tag.all
-  end
 
   def show
     @tag =  ActsAsTaggableOn::Tag.find(params[:id])
     @posts = Post.tagged_with(@tag.name)
   end
+
 end
