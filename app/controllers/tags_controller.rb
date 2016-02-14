a2c1d0d605cfc8babd4a1ffa0af7345d38aef0cb
TSMMark <mark.allen@vydia.com>
2016-02-14 11:15:46 -0500

    Paginated all pins

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
 
   def show
     @tag =  ActsAsTaggableOn::Tag.find_by_name(params[:id])
-    @pins = Pin.tagged_with(@tag.name).all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10).includes(:activities)
+    @pins = Pin.tagged_with(@tag.name).all.order("created_at DESC").paginate(:page => params[:page], :per_page => 30).includes(:activities)
   end
 
 end
