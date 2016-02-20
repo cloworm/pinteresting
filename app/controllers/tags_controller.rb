71b7b96ed5e1e33f1131a3d60dfcdc4ff67114c6
TSMMark <mark.allen@vydia.com>
2016-02-19 20:00:09 -0500

    Add breadcrumbs to tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,24 @@
 class TagsController < ApplicationController
+  include BreadcrumbsHelper
+
+  before_action :set_tag, only: [:show]
+
+  crumb(only: [:show]) do
+    ["Projects", pins_path]
+  end
+
+  crumb(only: [:show]) do
+    [@tag.name, tag_path(@tag.name)]
+  end
 
   def show
-    @tag =  ActsAsTaggableOn::Tag.find_by_name(params[:id])
     @pins = Pin.tagged_with(@tag.name).all.order("created_at DESC").paginate(:page => params[:page], :per_page => 30).includes(:activities)
   end
+
+  private
+
+    def set_tag
+      @tag = ActsAsTaggableOn::Tag.find_by_name(params[:id])
+    end
 
 end
