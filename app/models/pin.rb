6b620f1fd96f37c62e3f22a7cf35e391f7f7d628
TSMMark <mark.allen@vydia.com>
2016-02-20 11:02:53 -0500

    Delete activities and uploads when a pin is destroyed

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
-  has_many :activities
-  has_many :uploads
+  has_many :activities, :dependent => :destroy
+  has_many :uploads, :dependent => :destroy
 
   has_attached_file :image,
     styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" },
