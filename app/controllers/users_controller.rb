164f7cad2ea760615d320e24dd711679280269d4
TSMMark <mark.allen@vydia.com>
2016-02-20 12:55:40 -0500

    Move admin_logged_in helper to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @pins = @user.pins.all.paginate(:page => params[:page], :per_page => 20).includes(:activities)
+    @pins = @user.pins.all.paginate(:page => params[:page], :per_page => 10).includes(:activities)
     @pins = apply_pin_filters(@pins)
   end
 end
