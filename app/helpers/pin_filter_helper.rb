cbfa14ed9aa4fdf5becf8d44ef02778725c5ee9f
TSMMark <mark.allen@vydia.com>
2016-02-20 12:38:07 -0500

    Add anchor to user profile sorting

--- app/helpers/pin_filter_helper.rb
+++ app/helpers/pin_filter_helper.rb
@@ -1,13 +1,12 @@
 module PinFilterHelper
 
-  def sort_by_link
-    # TODO: dynamic path instead of always pins_path
+  def sort_by_link(options = {})
     if sort_by_most_liked?
-      most_recent = link_to "Most Recent", url_for(:filter => { :sort_by => "most_recent" })
-      most_liked = link_to "Most Liked", url_for(:filter => { :sort_by => "most_liked" }), class: "active-option"
+      most_recent = link_to "Most Recent", url_for({ :filter => { :sort_by => "most_recent" } }.merge(options))
+      most_liked = link_to "Most Liked", url_for({ :filter => { :sort_by => "most_liked" } }.merge(options)), class: "active-option"
     else
-      most_recent = link_to "Most Recent", url_for(:filter => { :sort_by => "most_recent" }), class: "active-option"
-      most_liked = link_to "Most Liked", url_for(:filter => { :sort_by => "most_liked" })
+      most_recent = link_to "Most Recent", url_for({ :filter => { :sort_by => "most_recent" } }.merge(options)), class: "active-option"
+      most_liked = link_to "Most Liked", url_for({ :filter => { :sort_by => "most_liked" } }.merge(options))
     end
 
     raw("Sort by: #{most_recent} | #{most_liked}")
