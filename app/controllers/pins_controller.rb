f6f9c1b28f1f821fa35aa568edd338d692bd45ca
TSMMark <mark.allen@vydia.com>
2016-02-20 11:31:18 -0500

    FIX ORDER

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -29,14 +29,14 @@ class PinsController < ApplicationController
 
     if sort_by_most_liked?
       subquery = Pin
-        .select("pins.id as pin_id, COALESCE(count(pins.id), 0) as likes_count")
+        .select("pins.id as pin_id, count(pins.id) as likes_count")
         .joins("INNER JOIN activities ON activities.pin_id = pins.id AND activities.type = 'like'")
         .group("pins.id")
         .order("likes_count DESC")
 
       @pins = @pins
         .joins("LEFT OUTER JOIN (#{subquery.to_sql}) as subquery ON pins.id = subquery.pin_id")
-        .order("subquery.likes_count DESC")
+        .order("COALESCE(subquery.likes_count, 0) DESC")
     else
       @pins = @pins.order("created_at DESC")
     end
