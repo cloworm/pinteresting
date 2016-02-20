48b86efb45594d663f79f5d819595eb237de4707
cloworm <rubyquin@gmail.com>
2016-02-20 12:43:02 -0500

    Refactor pin_filter_helper

--- app/helpers/pin_filter_helper.rb
+++ app/helpers/pin_filter_helper.rb
@@ -1,13 +1,11 @@
 module PinFilterHelper
 
   def sort_by_link(options = {})
-    if sort_by_most_liked?
-      most_recent = link_to "Most Recent", url_for({ :filter => { :sort_by => "most_recent" } }.merge(options))
-      most_liked = link_to "Most Liked", url_for({ :filter => { :sort_by => "most_liked" } }.merge(options)), class: "active-option"
-    else
-      most_recent = link_to "Most Recent", url_for({ :filter => { :sort_by => "most_recent" } }.merge(options)), class: "active-option"
-      most_liked = link_to "Most Liked", url_for({ :filter => { :sort_by => "most_liked" } }.merge(options))
-    end
+    most_recent_class = sort_by_most_liked? ? "" : "active-option"
+    most_liked_class = sort_by_most_liked? ? "active-option" : ""
+
+    most_recent = link_to "Most Recent", url_for({ :filter => { :sort_by => "most_recent" } }.merge(options)), class: most_recent_class
+    most_liked = link_to "Most Liked", url_for({ :filter => { :sort_by => "most_liked" } }.merge(options)), class: most_liked_class
 
     raw("Sort by: #{most_recent} | #{most_liked}")
   end
